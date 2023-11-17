c2e6f85ce37b484d64bdaf88fe4ce027050ad7fa
Pawel Januszewski <93876069+pjanuszewski@users.noreply.github.com>
2023-11-17 15:34:32 +0100

    Update3 ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -6,17 +6,18 @@ class ResourceManager
 {
 public:
     // Konstruktor domyślny
-    ResourceManager() : resource() {
+    ResourceManager() : resource(new Resource()) {
         // Inicjalizacja zarządzanego zasobu
     }
 
     // Destruktor
     ~ResourceManager() {
+        delete resource;
         // Zasób zostanie automatycznie zwolniony przy zniszczeniu ResourceManager
     }
 
     // Konstruktor kopiujący
-    ResourceManager(const ResourceManager& other) : resource(other.resource) {
+    ResourceManager(const ResourceManager& other) : resource(new Resource(*(other.resource))) {
         // Inicjalizacja kopii zarządzanego zasobu
     }
 
@@ -24,21 +25,24 @@ public:
     ResourceManager& operator=(const ResourceManager& other) {
         if (this != &other) {
             // Unikaj samoprzypisania
-            resource = other.resource; // Przypisanie kopii zarządzanego zasobu
+            delete resource;
+            resource = new Resource(*(other.resource)); // Przypisanie kopii zarządzanego zasobu
         }
         return *this;
     }
 
     // Konstruktor przenoszący
-    ResourceManager(ResourceManager&& other) noexcept : resource(std::move(other.resource)) {
-        // Inicjalizacja przeniesienia zarządzanego zasobu
+    ResourceManager(ResourceManager&& other) noexcept : resource(other.resource) {
+        other.resource = nullptr; // Przeniesienie własności
     }
 
     // Operator przypisania przenoszący
     ResourceManager& operator=(ResourceManager&& other) noexcept {
         if (this != &other) {
             // Unikaj samoprzypisania
-            resource = std::move(other.resource); // Przypisanie przeniesienia zarządzanego zasobu
+            delete resource;
+            resource = other.resource;
+            other.resource = nullptr; // Przeniesienie własności
         }
         return *this;
     }
@@ -46,14 +50,14 @@ public:
     // Metoda get
     double get() const {
         // Delegowanie wywołania do metody get zasobu
-        return resource.get();
+        return resource->get();
     }
 
-    // Getter dla zarządzanego zasobu (dodano const)
-    const Resource& getResource() const {
+    // Getter dla zarządzanego zasobu
+    Resource* getResource() const {
         return resource;
     }
 
 private:
-    Resource resource;  // Zarządzany zasób
+    Resource* resource;  // Zarządzany zasób
 };
